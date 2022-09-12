5fb3cdec6605dfe93bb0ee5ff6a231c96f14d80f
mirzavic4869 <avicenamirzam@gmail.com>
2022-09-12 11:20:45 +0700

    update link cv

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,6 @@
 import { React, useState } from "react";
 import Logo from "../assets/logoku.jpg";
-import {
-	FaBars,
-	FaTimes,
-	FaGithub,
-	FaLinkedin,
-	FaWhatsapp,
-} from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
@@ -17,12 +11,7 @@ const Navbar = () => {
 	return (
 		<div className="sticky top-0 w-full h-[80px] flex justify-between items-center px-4 bg-[#2c2c2cff] text-white shadow-md">
 			<div>
-				<img
-					className="rounded-full"
-					src={Logo}
-					alt="logo image"
-					style={{ width: "50px" }}
-				/>
+				<img className="rounded-full" src={Logo} alt="logo image" style={{ width: "50px" }} />
 			</div>
 			{/* menu */}
 
@@ -60,13 +49,7 @@ const Navbar = () => {
 			</div>
 			{/* mobile menu */}
 
-			<ul
-				className={
-					!nav
-						? "hidden"
-						: "absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center bg-slate-900"
-				}
-			>
+			<ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center bg-slate-900"}>
 				<li className="py-6 texlxt-4">
 					<Link onClick={handleClick} to="home" smooth={true} duration={500}>
 						Home
@@ -78,23 +61,12 @@ const Navbar = () => {
 					</Link>
 				</li>
 				<li className="py-6 texlxt-4">
-					<Link
-						onClick={handleClick}
-						to="skills"
-						hashSpy={true}
-						smooth={true}
-						duration={500}
-					>
+					<Link onClick={handleClick} to="skills" hashSpy={true} smooth={true} duration={500}>
 						Skills
 					</Link>
 				</li>
 				<li className="py-6 texlxt-4">
-					<Link
-						onClick={handleClick}
-						to="projects"
-						smooth={true}
-						duration={500}
-					>
+					<Link onClick={handleClick} to="projects" smooth={true} duration={500}>
 						Projects
 					</Link>
 				</li>
@@ -109,37 +81,25 @@ const Navbar = () => {
 			<div className="hidden fixed lg:flex flex-col top-[35%] left-0">
 				<ul>
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-[#ffb633ff]">
-						<a
-							className="flex justify-between w-full items-center"
-							href="https://www.linkedin.com/in/mirzam-avicena-601a71152/"
-						>
+						<a className="flex justify-between w-full items-center" href="https://www.linkedin.com/in/mirzam-avicena-601a71152/">
 							Linkedin
 							<FaLinkedin size={30} />
 						</a>
 					</li>
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-[#ffb633ff]">
-						<a
-							className="flex justify-between w-full items-center"
-							href="https://github.com/mirzavic4869"
-						>
+						<a className="flex justify-between w-full items-center" href="https://github.com/mirzavic4869">
 							GitHub
 							<FaGithub size={30} />
 						</a>
 					</li>
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-[#ffb633ff]">
-						<a
-							className="flex justify-between w-full items-center"
-							href="https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=avicenamirzam@gmail@gmail.com"
-						>
+						<a className="flex justify-between w-full items-center" href="https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=avicenamirzam@gmail@gmail.com">
 							Email
 							<HiOutlineMail size={30} />
 						</a>
 					</li>
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[10px] duration-300 bg-[#ffb633ff]">
-						<a
-							className="flex justify-between w-full items-center"
-							href="https://drive.google.com/file/d/1-OIcL0CbLLJSwfVuvO_jrPS4vKzohHXn/view?usp=sharing"
-						>
+						<a className="flex justify-between w-full items-center" href="https://drive.google.com/file/d/1ngk4tsoDrHHCTrCpGIcfa4IhpkMR91d2/view?usp=sharing">
 							Resume
 							<BsFillPersonLinesFill size={30} />
 						</a>
@@ -149,10 +109,7 @@ const Navbar = () => {
 			<div className="hidden fixed lg:flex flex-col bottom-0 right-0">
 				<ul>
 					<li className="w-[160px] h-[60px] flex justify-between items-center mr-[-100px] hover:mr-[10px] duration-300 bg-[#4fce5d]">
-						<a
-							className="flex justify-between w-full items-center"
-							href="https://wa.me/6282138793602"
-						>
+						<a className="flex justify-between w-full items-center" href="https://wa.me/6282138793602">
 							<FaWhatsapp size={30} />
 							WhatsApp
 						</a>
